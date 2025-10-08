6ee1d9e5f4e63f0adfc29fe745b97bd9c84bc4ae
NaviCo-de <aridarrell30@gmail.com>
2025-10-08 15:59:56 +0700

    Add templates for Sport Partner

--- Sport_Partner/models.py
+++ Sport_Partner/models.py
@@ -3,26 +3,29 @@ import uuid
 
 # Create your models here.
 class PartnerPost(models.Model):
-    KATEGORI_OLAHRAGA = [
-        ('sepakbola', 'Sepakbola'),
-        ('basket', 'Basket'),
-        ('tenis', 'Tenis'),
-        ('padel', 'Padel'),
-        ('bulu tangkis', 'Bulu Tangkis'),
-        ('jogging', 'Jogging')
+    CATEGORY_CHOICES = [
+        ('tennis', 'Tennis'),
+        ('basketball', 'Basketball'),
+        ('soccer', 'Soccer'),
+        ('badminton', 'Badminton'),
+        ('volleyball', 'Volleyball'),
+        ('paddle', 'Paddle'),
+        ('futsal', 'Futsal'),
+        ('table_tennis', 'Table Tennis'),
+        ('jogging', 'Jogging'),
     ]
 
     post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    judul = models.CharField(max_length=255)
-    deskripsi = models.TextField()
-    kategori = models.CharField(choices=KATEGORI_OLAHRAGA, default='sepakbola')
-    jam = models.TimeField()
-    lokasi = models.TextField()
-    total_pendaftar = models.PositiveIntegerField(default=0)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, default='sepakbola')
+    time = models.TimeField()
+    location = models.TextField()
+    total_participants = models.PositiveIntegerField(default=0)
 
     @property
-    def increment_total_pendaftar(self):
-        self.total_pendaftar += 1
+    def increment_total_participants(self):
+        self.total_participants += 1
         self.save()
 
     
