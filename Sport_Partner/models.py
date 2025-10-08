36200337b640ffbadf38e466830b7291b55cff5a
NaviCo-de <aridarrell30@gmail.com>
2025-10-08 15:26:01 +0700

    Add model for Sport_Partner

--- Sport_Partner/models.py
+++ Sport_Partner/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class PartnerPost(models.Model):
+    KATEGORI_OLAHRAGA = [
+        ('sepakbola', 'Sepakbola'),
+        ('basket', 'Basket'),
+        ('tenis', 'Tenis'),
+        ('padel', 'Padel'),
+        ('bulu tangkis', 'Bulu Tangkis'),
+        ('jogging', 'Jogging')
+    ]
+
+    post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    judul = models.CharField(max_length=255)
+    deskripsi = models.TextField()
+    kategori = models.CharField(choices=KATEGORI_OLAHRAGA, default='sepakbola')
+    jam = models.TimeField()
+    lokasi = models.TextField()
+    total_pendaftar = models.PositiveIntegerField(default=0)
+
+    @property
+    def increment_total_pendaftar(self):
+        self.total_pendaftar += 1
+        self.save()
+
+    
