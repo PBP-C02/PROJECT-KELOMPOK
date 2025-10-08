fb35758d5867641ce0dcc8ac8b35a24e7219d4c7
IkanBerkepalaLele <ehgojim@gmail.com>
2025-10-08 16:39:54 +0700

    edit model

--- Coach/models.py
+++ Coach/models.py
@@ -2,40 +2,27 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
+CATEGORY_CHOICES = [
+        ('tennis', 'Tennis'),
+        ('basketball', 'Basketball'),
+        ('soccer', 'Soccer'),
+        ('badminton', 'Badminton'),
+        ('volleyball', 'Volleyball'),
+        ('paddle', 'Paddle'),
+        ('futsal', 'Futsal'),
+        ('table_tennis', 'Table Tennis'),
+        ('swimming', 'Swimming'),
+    ]
+
 
 # Create your models here.
 class Coach(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
-    CATEGORY_CHOICES = [
-        ('tennis', 'Tennis'),
-        ('basketball', 'Basketball'),
-        ('soccer', 'Soccer'),
-        ('badminton', 'Badminton'),
-        ('volleyball', 'Volleyball'),
-        ('paddle', 'Paddle'),
-        ('futsal', 'Futsal'),
-        ('table_tennis', 'Table Tennis'),
-        ('swimming', 'Swimming'),
-    ]
-    
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    coach_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
+    description = description = models.TextField()
+    price = models.PositiveIntegerField(default=1)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES) 
     
     def __str__(self):
-        return self.name
-    
-class Service(models.Model):
-    coach = models.ForeignKey(Coach, on_delete=models.CASCADE, null=True)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    CATEGORY_CHOICES = [
-        ('tennis', 'Tennis'),
-        ('basketball', 'Basketball'),
-        ('soccer', 'Soccer'),
-        ('badminton', 'Badminton'),
-        ('volleyball', 'Volleyball'),
-        ('paddle', 'Paddle'),
-        ('futsal', 'Futsal'),
-        ('table_tennis', 'Table Tennis'),
-        ('swimming', 'Swimming'),
-    ]
-    price = models.PositiveIntegerField(default=1)
+        return self.name 
