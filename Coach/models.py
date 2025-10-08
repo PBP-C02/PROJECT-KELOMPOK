350ce122e8ac689797a2248be8fc961bfc871261
IkanBerkepalaLele <ehgojim@gmail.com>
2025-10-08 16:11:34 +0700

    init Coach models

--- Coach/models.py
+++ Coach/models.py
@@ -1,3 +1,41 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Coach(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
+    CATEGORY_CHOICES = [
+        ('tennis', 'Tennis'),
+        ('basketball', 'Basketball'),
+        ('soccer', 'Soccer'),
+        ('badminton', 'Badminton'),
+        ('volleyball', 'Volleyball'),
+        ('paddle', 'Paddle'),
+        ('futsal', 'Futsal'),
+        ('table_tennis', 'Table Tennis'),
+        ('swimming', 'Swimming'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+class Service(models.Model):
+    coach = models.ForeignKey(Coach, on_delete=models.CASCADE, null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    CATEGORY_CHOICES = [
+        ('tennis', 'Tennis'),
+        ('basketball', 'Basketball'),
+        ('soccer', 'Soccer'),
+        ('badminton', 'Badminton'),
+        ('volleyball', 'Volleyball'),
+        ('paddle', 'Paddle'),
+        ('futsal', 'Futsal'),
+        ('table_tennis', 'Table Tennis'),
+        ('swimming', 'Swimming'),
+    ]
+    price = models.PositiveIntegerField(default=1)
